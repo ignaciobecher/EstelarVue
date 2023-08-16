1c784a17b72811721eb83f9e10a0bf2a5cd05a48
Ignacio <ignacio.becher@gmail.com>
2023-08-16 10:18:37 -0300

    hero

--- src/components/headerPage.vue
+++ src/components/headerPage.vue
@@ -1,11 +1,25 @@
 <template>
-    <div class="hero-container">
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Bienvenido a nuestro sitio</h1>
-                <p class="lead">Explora nuestras increíbles características y servicios.</p>
-                <router-link to="/about" class="btn btn-primary btn-lg">Saber más</router-link>
+    <div class="container-fluid px-4 py-5 my-5 text-center">
+        <div class="lc-block mb-4">
+            <div editable="rich">
+                <h2 class="display-2 fw-bold">Transformando ideas <span class="text-primary">en realidad digital!</span>
+                </h2>
             </div>
+        </div>
+        <div class="lc-block col-lg-6 mx-auto mb-5">
+            <div editable="rich">
+
+                <p class="lead">Potenciamos tu presencia en línea a través de estrategias creativas de marketing, soluciones
+                    integrales de programacion y fotografia de primer nivel para tu negocio.</p>
+            </div>
+        </div>
+
+        <div class="lc-block d-grid gap-2 d-sm-flex justify-content-sm-center mb-5"> <a
+                class="btn btn-primary btn-lg px-4 gap-3" href="#" role="button">Quiero saber mas</a>
+            <a class="btn btn-outline-secondary btn-lg px-4" href="#" role="button">Contacto</a>
+        </div>
+        <div class="lc-block d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <img class="img-fluid" src="../assets/logoConLetras.png" width="" height="783" srcset="" sizes="" alt="">
         </div>
     </div>
 </template>
