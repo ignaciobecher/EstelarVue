ab9214a6bba52c18621dce263a663f729ba6d83f
Ignacio <ignacio.becher@gmail.com>
2023-08-16 11:27:55 -0300

    about color changed

--- src/components/aboutPage.vue
+++ src/components/aboutPage.vue
@@ -2,12 +2,11 @@
     <div class="container my-5" id="abouttextstyle">
         <div class="row">
             <div class="col-md-6 p-4 p-sm-5 order-2 order-sm-1">
-                <small class="text-uppercase" style="color: #9B5DE5;"></small>
-                <h1 class="h2 mb-4" style="font-weight: 600;">Our <span style="color: #9B5DE5;">mission</span></h1>
-                <p class="text-secondary" style="line-height: 2;">Catering to the individualistic needs of each student.<br>
-                    High quality learning experience based on a broad and balanced curriculum.
-                    To empower students utilise their full potential spiritually, intellectually, physically, socially and
-                    morally.
+                <small class="text-uppercase" style="color: #e85e00;"></small>
+                <h1 class="h2 mb-4" style="font-weight: 600;">Nuestra <span style="color: #e85e00;">mision</span></h1>
+                <p class="text-secondary" style="line-height: 2;"> Como empresa, los objetivos y logros que deseamos
+                    alcanzaar reflejan nuestra vision de exito y el impacto que queremos tener en nuestros clientes y en el
+                    mercado.
                 </p>
 
             </div>
@@ -20,14 +19,11 @@
                     class="w-100" alt="">
             </div>
             <div class="col-md-6 p-4 p-sm-5">
-                <small class="text-uppercase" style="color: #9B5DE5;"></small>
-                <h1 class="h2 mb-4" style="font-weight: 600;">About<span style="color: #9B5DE5;"> us</span></h1>
-                <p class="text-secondary" style="line-height: 2;">Maria Auxilium Girls’ Hr. Sec. School is situated at 15G
-                    Damalwar Street, Kancheepuram. It is set in a
-                    conducive atmosphere suitable for the growth of young minds. At Maria Auxilium School every child is
-                    given
-                    individual attention to ensure their holistic development. At MAS , all the needs of the child are met
-                    with understanding and utmost care, be it physical, emotional, intellectual or spiritual.</p>
+                <small class="text-uppercase" style="color: #e85e00;"></small>
+                <h1 class="h2 mb-4" style="font-weight: 600;">Acerca de<span style="color: #e85e00;"> nosotros</span></h1>
+                <p class="text-secondary" style="line-height: 2;"> Estelar marketing digital es una agencia de vanguardia en
+                    marketing digital para negocios que ofrece soluciones creativas y efectivas en el ambito del marketing
+                    digital y la publicidad, logrando que las emrpesas alcancen su maximo potencial.</p>
 
             </div>
         </div>
